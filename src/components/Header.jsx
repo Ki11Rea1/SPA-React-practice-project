b3371e18d5b02ce00bf9773c3f3ba33d2a6b59a7
Ki11Rea1 <kirill.z1799@gmail.com>
2022-03-28 18:09:40 +0300

    First commit! Modified app.js and app.css. Created new folder for components.

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.marketaccesscanada.ca/wp-content/uploads/2019/12/ACF-logo-placeholder-1.png" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div className="water">
+        <img src="https://mobimg.b-cdn.net/v3/fetch/1b/1b7900783b701ea5a5b73372e9cf0677.jpeg" />
+      </div>
+
+      <div>Avatar + description</div>
+
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
