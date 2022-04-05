c35825fa570205e189880239eed2c2cb4f8d945e
Ki11Rea1 <kirill.z1799@gmail.com>
2022-04-05 16:25:13 +0300

    <a> replaced with NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import style from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className={style.nav}>
       <div className={style.item}>
-        <a href="/profile">Profile</a>
+        <NavLink to="/profile">Profile</NavLink>
       </div>
       <div className={style.item}>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="/dialogs">Messages</NavLink>
       </div>
       <div className={style.item}>
-        <a href="/news">News</a>
+        <NavLink to="/news">News</NavLink>
       </div>
       <div className={style.item}>
-        <a href="/music">Music</a>
+        <NavLink to="/music">Music</NavLink>
       </div>
       <div className={style.item}>
-        <a href="/settings">Settings</a>
+        <NavLink to="/settings">Settings</NavLink>
       </div>
     </nav>
   );
