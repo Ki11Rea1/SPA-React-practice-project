34872f0555c2673bd23fd96fa69485471f38c39c
Ki11Rea1 <kirill.z1799@gmail.com>
2022-04-05 16:49:27 +0300

    NavLinks style tweaks

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,19 +6,54 @@ const Navbar = () => {
   return (
     <nav className={style.nav}>
       <div className={style.item}>
-        <NavLink to="/profile">Profile</NavLink>
+        <NavLink
+          to="/profile"
+          className={(navData) =>
+            navData.isActive ? style.active : style.item
+          }
+        >
+          Profile
+        </NavLink>
       </div>
       <div className={style.item}>
-        <NavLink to="/dialogs">Messages</NavLink>
+        <NavLink
+          to="/dialogs"
+          className={(navData) =>
+            navData.isActive ? style.active : style.item
+          }
+        >
+          Messages
+        </NavLink>
       </div>
       <div className={style.item}>
-        <NavLink to="/news">News</NavLink>
+        <NavLink
+          to="/news"
+          className={(navData) =>
+            navData.isActive ? style.active : style.item
+          }
+        >
+          News
+        </NavLink>
       </div>
       <div className={style.item}>
-        <NavLink to="/music">Music</NavLink>
+        <NavLink
+          to="/music"
+          className={(navData) =>
+            navData.isActive ? style.active : style.item
+          }
+        >
+          Music
+        </NavLink>
       </div>
       <div className={style.item}>
-        <NavLink to="/settings">Settings</NavLink>
+        <NavLink
+          to="/settings"
+          className={(navData) =>
+            navData.isActive ? style.active : style.item
+          }
+        >
+          Settings
+        </NavLink>
       </div>
     </nav>
   );
