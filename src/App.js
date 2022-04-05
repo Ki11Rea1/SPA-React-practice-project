f8a966bbb7b0441531102c7fdc1004737f163b70
Ki11Rea1 <kirill.z1799@gmail.com>
2022-04-05 14:00:51 +0300

    Added new component Dialogs with imports.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
   //Функция, которая возвращает JSX код, называется компонентой
@@ -11,7 +12,8 @@ const App = () => {
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <Profile />
+      {/* <Profile /> */}
+      <Dialogs />
     </div>
   );
 };
---
+++ src/components/Dialogs/Dialogs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dialogs = (props) => {
+  return <div>Dialogs</div>;
+};
+
+export default Dialogs;
