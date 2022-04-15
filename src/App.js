54cd2e65dfd7b92071d435ee7dbcb1582249d601
Ki11Rea1 <kirill.z1799@gmail.com>
2022-04-15 13:44:28 +0300

    All m,d,p data moved to index.js

--- src/App.js
+++ src/App.js
@@ -9,28 +9,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 
-const App = () => {
-  let postsData = [
-    { id: 1, message: "Hi, how are u?", likesCount: 2 },
-    { id: 2, message: "It`s my first post", likesCount: 5 },
-    { id: 3, message: "Cool! Keep learning JS and React!", likesCount: 8 },
-  ];
-
-  let dialogsData = [
-    { id: 1, name: "Kirill" },
-    { id: 2, name: "Ashot" },
-    { id: 3, name: "File" },
-    { id: 4, name: "Yasigara" },
-    { id: 5, name: "Link" },
-  ];
-
-  let messagesData = [
-    { id: 1, message: "Hi" },
-    { id: 2, message: "Hello" },
-    { id: 3, message: "Want to hang out?" },
-    { id: 4, message: "Sure" },
-  ];
-
+const App = (props) => {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
@@ -42,14 +21,14 @@ const App = () => {
               path="/dialogs/*"
               element={
                 <Dialogs
-                  dialogsData={dialogsData}
-                  messagesData={messagesData}
+                  dialogsData={props.dialogsData}
+                  messagesData={props.messagesData}
                 />
               }
             />
             <Route
               path="/profile"
-              element={<Profile postsData={postsData} />}
+              element={<Profile postsData={props.postsData} />}
             />
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+let postsData = [
+  { id: 1, message: "Hi, how are u?", likesCount: 2 },
+  { id: 2, message: "It`s my first post", likesCount: 5 },
+  { id: 3, message: "Cool! Keep learning JS and React!", likesCount: 8 },
+];
+
+let dialogsData = [
+  { id: 1, name: "Kirill" },
+  { id: 2, name: "Ashot" },
+  { id: 3, name: "File" },
+  { id: 4, name: "Yasigara" },
+  { id: 5, name: "Link" },
+];
+
+let messagesData = [
+  { id: 1, message: "Hi" },
+  { id: 2, message: "Hello" },
+  { id: 3, message: "Want to hang out?" },
+  { id: 4, message: "Sure" },
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      postsData={postsData}
+      dialogsData={dialogsData}
+      messagesData={messagesData}
+    />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
