895371523bbb0b2ae938aef73493e14a44499214
Ki11Rea1 <kirill.z1799@gmail.com>
2022-04-15 13:26:43 +0300

    postData moved to Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,10 +4,16 @@ import style from "./Profile.module.css";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
+  let postsData = [
+    { id: 1, message: "Hi, how are u?", likesCount: 2 },
+    { id: 2, message: "It`s my first post", likesCount: 5 },
+    { id: 3, message: "Cool! Keep learning JS and React!", likesCount: 8 },
+  ];
+
   return (
     <div>
       <ProfileInfo />
-      <MyPosts />
+      <MyPosts data={postsData} />
     </div>
   );
 };
