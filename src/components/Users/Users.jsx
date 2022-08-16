59e2423ebc3b16f49b4cd59a6055ac86b8087ca3
Ki11Rea1 <kirill.z1799@gmail.com>
2022-08-16 18:15:38 +0300

    User avatar picture was imported

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Users.module.css";
 import axios from "axios";
+import userAvatar from "../../Assets/user.png";
 
 let Users = (props) => {
   if (props.users.length === 0) {
@@ -16,7 +17,10 @@ let Users = (props) => {
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.avatar} className={styles.userPhoto} />
+              <img
+                src={u.photos.small != null ? u.photos.small : userAvatar}
+                className={styles.userPhoto}
+              />
             </div>
             <div>
               {u.followed ? (
