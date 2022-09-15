0156475490419de7b5b255f51d8813af23b2e6dc
Ki11Rea1 <kirill.z1799@gmail.com>
2022-09-15 15:14:37 +0300

    ProfileStatus

---
+++ src/components/Profile/ProfileInfo/ProfieStatus.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import style from "./ProfileInfo.module.css";
+
+class ProfileStatus extends React.Component {
+  state = {
+    editMode: false,
+  };
+
+  activateEditMode = () => {
+    this.setState({
+      editMode: true,
+    });
+  };
+
+  deactivateEditMode = () => {
+    this.setState({
+      editMode: false,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {!this.state.editMode && (
+          <div>
+            <span onDoubleClick={this.activateEditMode}>
+              {this.props.status}
+            </span>
+          </div>
+        )}
+        {this.state.editMode && (
+          <div>
+            <input
+              autoFocus={true}
+              onBlur={this.deactivateEditMode}
+              value={this.props.status}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default ProfileStatus;
