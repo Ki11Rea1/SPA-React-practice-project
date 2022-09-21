69e4afa685bc61fc478c9d9c3fe9e69a80f3852d
Ki11Rea1 <kirill.z1799@gmail.com>
2022-09-21 17:24:42 +0300

    reselect library installed

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -47,18 +47,6 @@ class UsersContainer extends React.Component {
   }
 }
 
-/* let mapStateToProps = (state) => {
-  return {
-    users: state.usersPage.users,
-    pageSize: state.usersPage.pageSize,
-    totalUsersCount: state.usersPage.totalUsersCount,
-    currentPage: state.usersPage.currentPage,
-    isFetching: state.usersPage.isFetching,
-    followingInProgress: state.usersPage.followingInProgress,
-  };
-};
- */
-
 let mapStateToProps = (state) => {
   return {
     users: getUsers(state),
