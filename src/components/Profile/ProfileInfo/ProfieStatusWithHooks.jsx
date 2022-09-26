327cad32ca81df73ac190d8a54bb2b53ad8c766a
Ki11Rea1 <kirill.z1799@gmail.com>
2022-09-26 15:33:26 +0300

    ProfileStatusWithHooks local state was synchronized with global state via useEffect hook

--- src/components/Profile/ProfileInfo/ProfieStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfieStatusWithHooks.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 
 const ProfileStatusWithHooks = (props) => {
   let [editMode, setEditMode] = useState(false);
   let [status, setStatus] = useState(props.status);
+
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
   const activateEditMode = () => {
     setEditMode(true);
