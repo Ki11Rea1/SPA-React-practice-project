65e72174e0b12b746e524e61b0d2efc900772e05
Ki11Rea1 <kirill.z1799@gmail.com>
2022-10-05 17:47:38 +0300

    App.js and index.js minor refactoring

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import MainApp from "./App";
 import App from "./App";
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(<MainApp />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
